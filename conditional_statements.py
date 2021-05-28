a196ab63a67292a5ffd32876a10083c8bbf372ee
Geethikakantheti <84315046+Geethikakantheti@users.noreply.github.com>
2021-05-28 11:08:43 +0530

    Add files via upload

--- conditional_statements.py
+++ conditional_statements.py
@@ -1,6 +1,10 @@
-userid=input()
-userpwd=input()
-if  userid==("geethu@gmail.com")  and  userpwd==("geethu@123"):
-    print("login success")
+m=int(input())
+if m>=80:
+    print("A grade")
+elif m<80 and m>=60:
+    print("b grade")
+elif m>=40 and m<60:
+    print("c grade")
 else:
-    print("login failed")
+    print("failed")
+    
