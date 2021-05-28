a52f2ee499bd50d1bd46012ac6d27341e9287218
Geethikakantheti <84315046+Geethikakantheti@users.noreply.github.com>
2021-05-28 18:32:32 +0530

    Add files via upload

---
+++ eliftest.py
@@ -0,0 +1,8 @@
+# Python code to illustrate elif statement 
+choice = input(f'Which game do you like, Press\nC - Cricket\nH - Hokey: ') 
+if choice == 'C': 
+                 print('You are a Cricketer!') 
+elif choice == 'H': 
+                 print('You are a Hockey player!') 
+else: 
+                 print('You are not interested in Sports')
