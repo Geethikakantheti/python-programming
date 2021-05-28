5e02bd2eb37ef34945827bbcca719fff6d5aef77
Geethikakantheti <84315046+Geethikakantheti@users.noreply.github.com>
2021-05-28 18:27:43 +0530

    Add files via upload

---
+++ dynamicwhile.py
@@ -0,0 +1,8 @@
+ #dynamic input for while loop
+
+count = int(input('How many times you want to say “PYTHON": '))
+i = 1 
+while i <= count: 
+                print('PYTHON') 
+                i += 1 
+print('Thank you!!')
